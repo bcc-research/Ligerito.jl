bf8afd434cfee17a9b3e914f9c615097fcacca47
Andrija <akinovak@gmail.com>
2025-05-01 00:47:12 +0200

    some readme stuff
    Co-authored-by: guille <angeris@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Ligerito
-implentation of Ligerito
+This is an experimental implementation of Ligerito polynomial commitment scheme
+described in [this paper](angeris.github.io/papers/ligerito.pdf) by Andrija
+Novakovic and Guillermo Angeris.
+
+**The code is not audited and is meant for research purposes only.**
+
+# How to run this code
+1. Clone the repository
+2. Launch a Julia REPL in the cloned directory
+3. Activate the environment (e.g., `using Pkg; Pkg.activate(".")`)
+4. Instantiate all dependencies (via `Pkg.instantiate()`)
+5. Run the example: (via `include("./examples/prove_verify.jl")`)
+
+We provide multiple possible configurations. For more, see the
+`examples/prove.jl` and `src/configs.jl` files.
+
+# Note
+We plan to upstream this into the `bcc-research/CryptoUtilitiesExamples`
+repository and make this slightly more modular for use in future
+implementations, but the current code serves as a reference and mirrors the
+description of the algorithm in the Ligerito paper.
---
+++ examples/prove_verify.jl
@@ -0,0 +1,30 @@
+using BinaryFields, StatsBase
+using Ligerito
+
+# We provide multiple 'hardcoded' configurations such as
+# hardcoded_config_20
+# hardcoded_config_24
+# hardcoded_config_28
+# hardcoded_config_30
+# Note that this _must_ match the verifier configuration and the size of the
+# vector `poly` containing the polynomial coefficients.
+config = Ligerito.hardcoded_config_24(BinaryElem32, BinaryElem128)
+poly = rand(BinaryElem32, 2^24)
+
+@info "Running with $(Threads.nthreads()) threads"
+@time proof = prover(config, poly)
+
+proof_size = sizeof(proof)
+@info "Proof size: $(Base.format_bytes(proof_size))"
+
+# We provide multiple 'hardcoded' configurations such as
+# hardcoded_config_20_verifier()
+# hardcoded_config_24_verifier()
+# hardcoded_config_28_verifier()
+# hardcoded_config_30_verifier()
+# Note that this _must_ match the verifier configuration and the size of the
+# vector `poly` containing the polynomial coefficients.
+verifier_cfg = Ligerito.hardcoded_config_24_verifier()
+@time verification_res = verifier(verifier_cfg, proof)
+println("Verification result: ", verification_res)
+@assert verification_res == true
